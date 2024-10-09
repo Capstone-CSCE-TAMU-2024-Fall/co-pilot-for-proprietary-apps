cf9e2e7d740fb65fc6e9b76579563eec693bf96e
Steven7zzz <q1824711081@gmail.com>
2024-10-08 22:25:13 -0500

    added logic to toggle code insertion

--- com.plugin.copilotassistant/src/com/plugin/copilotassistant/handlers/SampleHandler.java
+++ com.plugin.copilotassistant/src/com/plugin/copilotassistant/handlers/SampleHandler.java
@@ -3,6 +3,7 @@ package com.plugin.copilotassistant.handlers;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.ITextSelection;
 import org.eclipse.swt.custom.StyleRange;
@@ -10,6 +11,7 @@ import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.eclipse.ui.texteditor.ITextEditor;
 
@@ -19,6 +21,11 @@ public class SampleHandler extends AbstractHandler {
     public Object execute(ExecutionEvent event) throws ExecutionException {
         IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
         IEditorPart editor = window.getActivePage().getActiveEditor();
+        
+        // Retrieve the preference value from the preference store
+        IPreferenceStore preferenceStore = PlatformUI.getPreferenceStore();
+        boolean enabled = preferenceStore.getBoolean("ENABLE_INSERTION"); // Retrieve the value of the toggle button
+
 
         if (editor instanceof ITextEditor) {
             ITextEditor textEditor = (ITextEditor) editor;
@@ -28,7 +35,15 @@ public class SampleHandler extends AbstractHandler {
                 ITextSelection selection = (ITextSelection) textEditor.getSelectionProvider().getSelection();
                 int offset = selection.getOffset();
                 String textToInsert = "Done";
+                
+                if (!enabled) {
+                	document.replace(offset, 0, "$");
+                	return null;
+                }
+
                 document.replace(offset, 0, textToInsert);
+
+            
                 
              // Get the StyledText widget
                 StyledText styledText = (StyledText) textEditor.getAdapter(org.eclipse.swt.widgets.Control.class);
