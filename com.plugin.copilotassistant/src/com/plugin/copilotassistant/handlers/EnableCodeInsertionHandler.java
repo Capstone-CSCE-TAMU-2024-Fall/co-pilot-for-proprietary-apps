0155f8124745c35fe4ff2e4101341769c17c8572
Steven7zzz <q1824711081@gmail.com>
2024-11-05 15:53:22 -0600

    use two different pngs to represent two states of the toggle button

--- com.plugin.copilotassistant/src/com/plugin/copilotassistant/handlers/EnableCodeInsertionHandler.java
+++ com.plugin.copilotassistant/src/com/plugin/copilotassistant/handlers/EnableCodeInsertionHandler.java
@@ -1,16 +1,24 @@
 package com.plugin.copilotassistant.handlers;
 
+import java.util.Map;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.Command;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.State;
+import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.commands.ICommandService;
+import org.eclipse.ui.commands.IElementUpdater;
+import org.eclipse.ui.menus.UIElement;
 
 
-public class EnableCodeInsertionHandler extends AbstractHandler {
-
+public class EnableCodeInsertionHandler extends AbstractHandler implements IElementUpdater {
+	
+	private static final String ENABLED_ICON_PATH = "/icons/toggle_on.png";
+    private static final String DISABLED_ICON_PATH = "/icons/toggle_off.png";
+    
 
     @Override
     public Object execute(ExecutionEvent event) throws ExecutionException {      
@@ -22,9 +30,34 @@ public class EnableCodeInsertionHandler extends AbstractHandler {
         boolean isEnabled = !(Boolean) state.getValue();
         state.setValue(isEnabled);
         
-//        String message = isEnabled ? "Code Insertion Enabled" : "Code Insertion Disabled";
-//        MessageDialog.openInformation(shell, "Code Insertion", message);
+        // Refresh the UI to reflect the state change
+        commandService.refreshElements("com.plugin.copilotassistant.commands.enableCodeInsertion", null);
+
 
         return null;
+    }
+    
+    @Override
+    public void updateElement(UIElement element, Map parameters) {
+        ICommandService commandService = (ICommandService) PlatformUI.getWorkbench().getService(ICommandService.class);
+        Command command = commandService.getCommand("com.plugin.copilotassistant.commands.enableCodeInsertion");
+        State state = command.getState("org.eclipse.ui.commands.toggleState");
+
+        boolean isEnabled = (Boolean) state.getValue();
+
+        // Set the icon based on the state
+        String iconPath = isEnabled ? ENABLED_ICON_PATH : DISABLED_ICON_PATH;
+        
+        ImageDescriptor icon = ImageDescriptor.createFromFile(EnableCodeInsertionHandler.class, iconPath);
+
+        if (icon == null) {
+            System.out.println("Failed to load icon: " + iconPath);
+        }
+        
+        element.setIcon(icon);
+
+        // Optionally, update tooltip text to indicate the current state
+        String tooltip = isEnabled ? "Disable Code Insertion" : "Enable Code Insertion";
+        element.setTooltip(tooltip);
     }
 }
